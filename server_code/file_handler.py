74b2f4d7c3ab94d3dd88332bde528901c43d9aa2
Bruce Fraser <bruce.fraserb@gmail.com>
2026-01-23 09:22:06 +0200

    New feature - connected Github

--- server_code/file_handler.py
+++ server_code/file_handler.py
@@ -3,15 +3,7 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
 
-# This is a server module. It runs on the Anvil server,
-# rather than in the user's browser.
-#
-# To allow anvil.server.call() to call functions here, we mark
-# them with @anvil.server.callable.
-# Here is an example - you can replace it with your own:
-#
-# @anvil.server.callable
-# def say_hello(name):
-#   print("Hello, " + name + "!")
-#   return 42
-#
+@anvil.server.callable
+def save_file(file, description):
+    """Saves a file along with its description to the database."""
+    app_tables.Flights.add_row(file=file, description=description)
