ec3a1efd059d3d9c8e585338a075a4fccca9e3f5
Bruce Fraser <bruce.fraserb@gmail.com>
2026-01-24 22:50:25 +0200

    visual kickoff with corect dashboard form updaetd

--- client_code/Homepage/dashboards/__init__.py
+++ client_code/Homepage/dashboards/__init__.py
@@ -4,6 +4,7 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from ..graph_data import create_graphs
 
 
 class dashboards(dashboardsTemplate):
@@ -11,13 +12,26 @@ class dashboards(dashboardsTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     self.load_controls()
-    self.cp_controls.set_event_handler('x-listen',self.act)
+    self.cp_controls.set_event_handler('x-listen', self.act)
 
-  def load_controls(self,**event_args):
+  def load_controls(self, **event_args):
+    """
+    Load the controller into the cp_controls container.
+    """
     from ..comp_controls import comp_controls
     self.controller = comp_controls()
     self.cp_controls.add_component(self.controller)
 
-  def act(self,package,**event_args):
-    # Co-pilot please start here for dashboards form
-    pass
+  def act(self, package, **event_args):
+    """
+    Generate graphs using the package and place them in the graphs_panel.
+    """
+    # Clear the graphs panel
+    self.graphs_panel.clear()
+
+    # Generate graphs using the graph_data module
+    graphs = create_graphs(package)
+
+    # Add each graph to the graphs panel
+    for graph in graphs:
+      self.graphs_panel.add_component(PlotlyPanel(graph))
