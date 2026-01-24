d66b0cce3b1465a98f402f2b8c9dc8ca54e17606
bruce.fraserb@gmail.com <bruce.fraserb@gmail.com>
2026-01-24 20:33:02 +0000

    Created graph_data; Edited Homepage.dashboards

--- client_code/Homepage/dashboards/__init__.py
+++ client_code/Homepage/dashboards/__init__.py
@@ -10,5 +10,13 @@ class dashboards(dashboardsTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.load_controls()
+    self.cp_controls.set_event_handler('x-listen',self.act)
 
-    # Any code you write here will run before the form opens.
+  def load_controls(self,**event_args):
+    from ..comp_controls import comp_controls
+    self.controller = comp_controls()
+    self.cp_controls.add_component(self.controller)
+
+  def act(self,package,**event_args):
+    pass
